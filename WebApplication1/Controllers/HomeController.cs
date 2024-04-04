1e23fd66f9b00353d4881e64da75166c20338c0c
Aniket Gedam <aniket.gedam@rws.com>
2024-04-04 14:07:43 +0530

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult ConvertToCsv()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SingleFileUpload(IFormFile SingleFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (SingleFile != null && SingleFile.Length > 0)
+                {
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", SingleFile.FileName);
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await SingleFile.CopyToAsync(stream);
+                    }
+
+                    string fileName = Path.GetFileName(SingleFile.FileName);
+                    return View("ConvertToCsv", fileName);
+                }
+            }
+            return View("ConvertToCsv");
+        }
+
+        public FileResult DownloadFile(string fileName)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", fileName);
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/octet-stream", fileName);
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
